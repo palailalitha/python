1873afbf423f1a0c472027c56e5c9db7730f56b8
palailalitha <41625916+palailalitha@users.noreply.github.com>
2022-09-04 19:59:57 +0530

    Add files via upload

---
+++ fizzbuzz.py
@@ -0,0 +1,12 @@
+def fizbuz(n):
+    for i in range(1,n+1):
+        if i%3==0 and i%5==0:
+            print(i,"FizzBuzz")
+        elif i % 3 == 0:
+                print(i,"Fizz")
+        elif i%5 ==0:
+                    print(i,"Buzz")
+        else: 
+            print(i)
+                    
+fizbuz(15)
